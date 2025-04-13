bad346992b74e5305a4fd88ef5ad2381db6f19cb
Reynier124 <reynier1306@gmail.com>
2025-04-13 08:58:07 -0300

    Añadido mapa en gestor de usuario

--- TP_2/src/Util/GestorUsuarios.java
+++ TP_2/src/Util/GestorUsuarios.java
@@ -2,30 +2,28 @@ package Util;
 
 import Interface.ServicioNotificaciones;
 import Usuario.Usuario;
-import Util.Input;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class GestorUsuarios {
-    private List<Usuario> usuarios;
-    private Scanner sc;
-    private Input ip;
+    //private List<Usuario> usuarios;
+    private Map<String, Usuario> usuarios;
+    private final Scanner sc;
+    private final Input ip;
     private final ServicioNotificaciones notificaciones;
 
     public GestorUsuarios(Scanner sc, ServicioNotificaciones notificaciones) {
-        usuarios = new ArrayList<>();
+        usuarios = Collections.emptyMap();
         this.sc = sc;
         this.ip = new Input(sc);
         this.notificaciones = notificaciones;
     }
 
-    public List<Usuario> getUsuarios() {
+    public Map<String, Usuario> getUsuarios() {
         return usuarios;
     }
 
-    public void setUsuarios(List<Usuario> usuarios) {
+    public void setUsuarios(Map<String, Usuario> usuarios) {
         this.usuarios = usuarios;
     }
 
@@ -35,7 +33,8 @@ public class GestorUsuarios {
         String email = ip.leerTexto("Email: ");
         String password = ip.leerTexto("Password: ");
         Usuario nuevo = new Usuario(nombre, email, password);
-        usuarios.add(nuevo);;
+        usuarios.put(nombre, nuevo);
+        //usuarios.add(nuevo);;
         notificaciones.notificar("Usuario creado: " + nuevo);
     }
 }
